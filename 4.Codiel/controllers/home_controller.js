90fa5ea905f8fcf82e8b41bf65b9a63afa1abf16
Vijay Choudhary <scriptyvijay@gmail.com>
2023-05-04 23:36:45 +0530

    added files

---
+++ 4.Codiel/controllers/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home = function (req, res) {
+	return;
+
+	res.send("<h1>Controller is up for controller</h1>");
+};
--- 4.Codiel/index.js
+++ 4.Codiel/index.js
@@ -2,5 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// Adding Express Router
+app.use("/", require("./routes"));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Codiel listening on port ${port}!`));
